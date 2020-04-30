37359f84e657dd548e20c9107ef8678280756bb7
SeekyCt <58006653+SeekyCt@users.noreply.github.com>
2020-04-30 18:37:51 +0100

    Changed vtable notation

--- itemslotdata.h
+++ itemslotdata.h
@@ -9,6 +9,7 @@ public:
   virtual ~PlayerHolderSub(); // 8021a144 PAL
   PlayerHolderSub(); // 8021a0f0 PAL
   
+  // vtable 802a2b48 PAL
   void * unknown_0x4;
   void * unknown_0x8;
   void * unknown_0xC;
@@ -19,13 +20,14 @@ typedef struct {
   short * data;
 } ItemSlotTableHolder;
 
-class ItemSlotData : public PlayerHolderSub { // 808d27b4 vtable
+class ItemSlotData : public PlayerHolderSub {
 public:
   virtual ~ItemSlotData(); // 807ba650 PAL
   int decideItem(int itemboxSetting, int position, int r6, int r7, void * r8); // 807bb42c PAL
   byte * processTableAndIter(byte * itemTable, ItemSlotTableHolder tableHolder, int r6, int r7); // 807ba9d8 PAL, takes a pointer to a raw table in an item slot file, processes the values and writes them to the data field of the table holder, then returns the pointer to the start of the next table
   // Always constructed inline
   
+  // vtable 808d27b4 PAL
   ItemSlotTableHolder playerChances;
   ItemSlotTableHolder cpuChances;
   ItemSlotTableHolder specialChances;
@@ -36,5 +38,5 @@ public:
   
   // unknown 0x30-33
   unsigned int[4] unknown_0x34; // can't remember why I thought this was an array, might be wrong
-  unsigned int playerCount; // copied from the global variable by constructor
+  unsigned int playerCount; // copied from the global variable by constructor, used to check when the player count changes online
 };
